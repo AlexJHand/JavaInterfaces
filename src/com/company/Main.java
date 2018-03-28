2ce314a91982c81e4209ce03bcd6830b8d71ed4f
Alex Hand <alex.j.hand@gmail.com>
2018-03-28 16:19:38 -0500

    Instantiated new phone

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,10 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    ITelephone alexsPhone;
+	    alexsPhone = new DeskPhone(55512345);
+	    alexsPhone.powerOn();
+	    alexsPhone.callPhone(55512345);
+	    alexsPhone.answer();
     }
 }
